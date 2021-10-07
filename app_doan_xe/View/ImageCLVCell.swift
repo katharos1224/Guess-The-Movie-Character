a2e046646a6993d65ca793cfdbe69a4eed5af508
Le Quang Son <lequangson2412@gmail.com>
2021-10-08 03:08:07 +0700

    no message

---
+++ app_doan_xe/View/ImageCLVCell.swift
@@ -0,0 +1,18 @@
+//
+//  ImageCLVCell.swift
+//  app_doan_xe
+//
+//  Created by Son on 07/10/2021.
+//
+
+import UIKit
+
+class ImageCLVCell: UICollectionViewCell {
+
+    @IBOutlet weak var imgView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
